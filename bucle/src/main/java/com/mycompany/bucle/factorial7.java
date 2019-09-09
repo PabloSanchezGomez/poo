be1ce36b6138a725d50d4325b8ca0f8a8727c06e
PabloSanchezGomez <32239065+PabloSanchezGomez@users.noreply.github.com>
2019-09-09 14:10:39 +0200

    Create factorial7.java

---
+++ bucle/src/main/java/com/mycompany/bucle/factorial7.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.bucle;
+
+/**
+ *
+ * @author pablo
+ */
+public class factorial7 {
+        public static void main(String[] args ){
+            int t = 1;
+            for(int x = 1;x<=7;x++){
+                t = t*x;
+            }
+            System.out.println(t + "\n");
+}
+}
