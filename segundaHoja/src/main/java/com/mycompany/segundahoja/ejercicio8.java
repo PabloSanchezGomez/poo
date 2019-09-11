281702dc7725ed9ad2f8b19ea7271d8675147b7e
PabloSanchezGomez <32239065+PabloSanchezGomez@users.noreply.github.com>
2019-09-11 12:30:48 +0200

    .

--- segundaHoja/src/main/java/com/mycompany/segundahoja/ejercicio8.java
+++ segundaHoja/src/main/java/com/mycompany/segundahoja/ejercicio8.java
@@ -9,7 +9,8 @@ package com.mycompany.segundahoja;
  *
  * @author ORDENADOR PABLO
  */
-public class ejercicio8 {    public static void main(String [] arg){
+public class ejercicio8 { 
+    public static void main(String [] arg){
         for(int i = 10;i>=1;i--){
             System.out.println();
             for(int j = i;j!=0;j--){
--- segundaHoja/src/main/java/com/mycompany/segundahoja/ejercicio9.java
+++ segundaHoja/src/main/java/com/mycompany/segundahoja/ejercicio9.java
@@ -10,5 +10,20 @@ package com.mycompany.segundahoja;
  * @author ORDENADOR PABLO
  */
 public class ejercicio9 {
-    
+        public static void main(String [] arg){
+            
+            for(int i = 10;i>=1;i--){
+            System.out.println();
+            for(int k = 0;k<=10-i;k++){
+                if(k>=1){
+                System.out.print(" ");
+                }
+                
+            }
+            for(int j = i;j!=0;j--){
+                System.out.print("*");
+            
+        }
+    }
+        }
 }
