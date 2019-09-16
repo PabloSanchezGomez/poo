06dcfb88509ba15b7dcbcb85d7b0198a631da2b9
PabloSanchezGomez <32239065+PabloSanchezGomez@users.noreply.github.com>
2019-09-16 18:19:48 +0200

    Update Punto.java

--- proyecto1/src/main/java/com/mycompany/proyecto1/Punto.java
+++ proyecto1/src/main/java/com/mycompany/proyecto1/Punto.java
@@ -24,14 +24,42 @@ public class Punto {
     
     public void desplazar(int x, int y)
     {
-        this.x = this.x+x;
-        this.x = this.x+y;
+        this.setX(this.getX() + x);
+        this.setX(this.getX() + y);
     }
     public int coordenadaX(){
-        return x;
+        return getX();
     }
     public int coordenadaY(){
+        return getY();
+    }
+
+    /**
+     * @return the x
+     */
+    public int getX() {
+        return x;
+    }
+
+    /**
+     * @param x the x to set
+     */
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    /**
+     * @return the y
+     */
+    public int getY() {
         return y;
     }
-    
+
+    /**
+     * @param y the y to set
+     */
+    public void setY(int y) {
+        this.y = y;
+    }
+  
 }
