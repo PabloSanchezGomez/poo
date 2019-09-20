6e8019479fa1886ee110972905afd20460af9c22
PabloSanchezGomez <32239065+PabloSanchezGomez@users.noreply.github.com>
2019-09-20 10:21:27 +0200

    Create ejercicio9.java

---
+++ segundaHoja/src/main/java/com/mycompany/segundahoja/ejercicio9.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.segundahoja;
+
+/**
+ *
+ * @author ORDENADOR PABLO
+ */
+public class ejercicio9 {
+        public static void main(String [] arg){
+<<<<<<< HEAD
+        for(int i = 1;i<=10;i++){
+            System.out.println();
+            for(int k = 0;k<=9;k++){
+                
+            for(int j = i;j!=0;j--){
+                System.out.print("*");
+            }
+            }
+        }
+    }
+    
+=======
+            
+            for(int i = 10;i>=1;i--){
+            System.out.println();
+            for(int k = 0;k<=10-i;k++){
+                if(k>=1){
+                System.out.print(" ");
+                }
+                
+            }
+            for(int j = i;j!=0;j--){
+                System.out.print("*");
+            
+        }
+    }
+        }
+>>>>>>> 06dcfb88509ba15b7dcbcb85d7b0198a631da2b9
+}
