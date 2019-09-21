31b67fba32b808df0b4dd9c675d4b38e3b512229
PabloSanchezGomez <32239065+PabloSanchezGomez@users.noreply.github.com>
2019-09-21 22:26:35 +0200

    Update Fraccion.java

--- proyecto1/src/main/java/com/mycompany/proyecto1/Fraccion.java
+++ proyecto1/src/main/java/com/mycompany/proyecto1/Fraccion.java
@@ -11,25 +11,45 @@ package com.mycompany.proyecto1;
  */
 public class Fraccion {
     private int num,dem;
+    private int mcd(int a, int b){
+       int resultado;
+       if (b == 0){
+           resultado = a;
+       }
+       else{
+           resultado = mcd(b, a % b);
+       }
+       return resultado;
     
-    public void Fraccion(){
+    }
+    
+    public Fraccion(){
         setNum(1);
         setDem(1);
     }
-   public void Fraccion(int x){
+   public Fraccion(int x){
         setNum(x);
         setDem(1);
    }
-   public void Fraccion(int x,int y){
+   public  Fraccion(int x,int y){
+       if (dem != 0){
         setNum(x);
         setDem(y);
+       }
+       else{
+           setNum(0);
+           setDem(0);
+       }
    }
-   public void Fra
+   public Fraccion(Fraccion f){
+       this.setDem(f.getDem());
+       this.setNum(f.getNum());
+   }
 
     /**
      * @return the num
      */
-    public int getNum() {
+    public int getNum(){
         return num;
     }
 
@@ -53,5 +73,58 @@ public class Fraccion {
     public void setDem(int dem) {
         this.dem = dem;
     }
+    public Fraccion suma(Fraccion f){
+        int a,b;
+        a = this.getNum()*f.getDem() + this.getDem()*f.getNum();
+        b = this.getDem()*f.getDem();
+        Fraccion result = new Fraccion(a,b);
+        return result;
+    }
+    public Fraccion resta(Fraccion f){
+        int a,b;
+        a = this.getNum()*f.getDem() - this.getDem()*f.getNum();
+        b = this.getDem()*f.getDem();
+        Fraccion result = new Fraccion(a,b);
+        return result;
+    }
+    public Fraccion multiplicar(Fraccion f){
+        int a,b;
+        a = this.getNum()*f.getNum();
+        b = this.getDem()*f.getDem();
+        Fraccion result = new Fraccion(a,b);
+        return result;       
+    }
+    public Fraccion dividir(Fraccion f){
+        int a,b;
+        a = this.getNum()*f.getDem();
+        b = this.getDem()*f.getNum();
+        Fraccion result = new Fraccion(a,b);
+        return result;       
+    }
+    public void monstrar(){
+        System.out.println(this.getNum()+("/")+this.getDem());
+    }
+    public int entero(){
+        int n = this.getNum()/this.getDem();
+        return n;
+    }
+    public double real(){
+        double n = this.getNum()/this.getDem();
+        return n;
+    }
+    public boolean irreducible(){
+        return(mcd(this.getNum(),this.getDem())==1); 
+    }
+    public Fraccion copia(){
+       return new Fraccion(this.getNum(),this.getDem());
+        
+    }
+    public boolean equivalente(Fraccion f){
+        return((this.getNum()/this.getDem()) == f.getNum()/f.getDem());
+    }
+    public void simplifica(){
+        int mcd = mcd(getNum(),getDem());
+    this.setNum(num / mcd);
+    this.setDem(dem / mcd);}
     
 }
