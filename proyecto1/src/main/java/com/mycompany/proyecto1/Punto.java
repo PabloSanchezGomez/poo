ff5e190e9fd8b1d17ab028c8270309f96b483233
PabloSanchezGomez <32239065+PabloSanchezGomez@users.noreply.github.com>
2019-09-21 13:34:39 +0200

    end Punto

--- proyecto1/src/main/java/com/mycompany/proyecto1/Punto.java
+++ proyecto1/src/main/java/com/mycompany/proyecto1/Punto.java
@@ -27,6 +27,11 @@ public class Punto {
         this.setX(this.getX() + x);
         this.setX(this.getX() + y);
     }
+    public void desplazar(int x)
+    {
+        this.setX(this.getX() + x);
+        
+    }
     public int coordenadaX(){
         return getX();
     }
@@ -61,5 +66,38 @@ public class Punto {
     public void setY(int y) {
         this.y = y;
     }
-  
+    void posicion(){
+        System.out.println(x + (",")+y);
+    }
+    Punto copia(){
+       Punto p2 = new Punto(x,y);
+       return p2;
+    }
+    int distancia(Punto p2){
+        int x1 = this.getX();
+        int x2 = p2.getX();
+        int y1 = this.getY();
+        int y2 = p2.getY();
+        return  (Math.abs(x1-x2)+ Math.abs(y1+y2));
+    }
+    
+    public static void main(String [] arg){
+         Punto p1 = new Punto (); 
+         Punto p2 = new Punto (5,5);
+         Punto p3 = p1.copia();
+         p1.desplazar(5);
+         System.out.print("p1: ");
+         p1.posicion();
+         p2.desplazar(3,7);
+         System.out.print ("p2: ");
+         p2.posicion();
+         System.out.print ("p3: ");
+         p3.posicion();
+         p3.setX(4);
+         p3.setY(7);
+         int x = p3.coordenadaX();
+         int y = p3.coordenadaY();
+         System.out.print ("p4: ");
+         System.out.println(x + (",") + y);
+    }
 }
