4154fa4f8220a177e55faa2fe897f5ead7035c53
PabloSanchezGomez <32239065+PabloSanchezGomez@users.noreply.github.com>
2019-09-21 13:44:24 +0200

    Update Fraccion.java

--- proyecto1/src/main/java/com/mycompany/proyecto1/Fraccion.java
+++ proyecto1/src/main/java/com/mycompany/proyecto1/Fraccion.java
@@ -10,5 +10,48 @@ package com.mycompany.proyecto1;
  * @author ORDENADOR PABLO
  */
 public class Fraccion {
+    private int num,dem;
+    
+    public void Fraccion(){
+        setNum(1);
+        setDem(1);
+    }
+   public void Fraccion(int x){
+        setNum(x);
+        setDem(1);
+   }
+   public void Fraccion(int x,int y){
+        setNum(x);
+        setDem(y);
+   }
+   public void Fra
+
+    /**
+     * @return the num
+     */
+    public int getNum() {
+        return num;
+    }
+
+    /**
+     * @param num the num to set
+     */
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    /**
+     * @return the dem
+     */
+    public int getDem() {
+        return dem;
+    }
+
+    /**
+     * @param dem the dem to set
+     */
+    public void setDem(int dem) {
+        this.dem = dem;
+    }
     
 }
