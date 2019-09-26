9f5c8f8d00f4290b457a54ce6991f7a106e58710
PabloSanchezGomez <32239065+PabloSanchezGomez@users.noreply.github.com>
2019-09-26 22:17:15 +0200

    .

---
+++ trabajos4-5/src/test/java/RectanguloTest.java
@@ -0,0 +1,43 @@
+
+import com.mycompany.trabajos4.Rectangulo;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class RectanguloTest {
+    Rectangulo r;
+        
+    public RectanguloTest() {
+        r = new Rectangulo();
+    }
+
+    @org.junit.jupiter.api.BeforeAll
+    public static void setUpClass() throws Exception {
+    }
+
+    @org.junit.jupiter.api.AfterAll
+    public static void tearDownClass() throws Exception {
+    }
+
+    @org.junit.jupiter.api.BeforeEach
+    public void setUp() throws Exception {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    public void tearDown() throws Exception {
+    }
+    public void testBase(){
+        assertEquals("1",1,r.base());
+    }
+
+    private void assertEquals(String string, int i, int base) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+
+
+}
